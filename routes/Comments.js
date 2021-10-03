bb369e751f4b6978e0acfe42b13795ae73252d18
gabriel6181997 <gabriel6181997@gmail.com>
2021-10-03 20:43:59 +0900

    ✨ Add router for deleting comments

--- routes/Comments.js
+++ routes/Comments.js
@@ -3,21 +3,26 @@ const router = express.Router();
 const { Comments } = require("../models");
 const { validateToken } = require("../middleware/AuthMiddleWare");
 
-router.get("/:postId", async(req, res)=> {
-  const postId = req.params.postId
+router.get("/:postId", async (req, res) => {
+  const postId = req.params.postId;
   const comments = await Comments.findAll({
-    where: {PostId: postId},
-  })
+    where: { PostId: postId },
+  });
   res.json(comments);
-})
+});
 
-router.post("/", validateToken, async(req, res)=> {
-  const comment = req.body
-  const username = req.user.username
-  comment.username = username
-  await Comments.create(comment)
+router.post("/", validateToken, async (req, res) => {
+  const comment = req.body;
+  const username = req.user.username;
+  comment.username = username;
+  await Comments.create(comment);
   res.json(comment);
-})
+});
 
+router.delete("/:commentId", validateToken, async (req, res) => {
+  const commentId = req.params.commentId;
+  await Comments.destroy({ where: { id: commentId } });
+  res.json("Deleted successfully!");
+});
 
 module.exports = router;
