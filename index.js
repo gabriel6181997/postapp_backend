7ff6f623ed51525e39f834c728c0c77fe330b675
gabriel6181997 <gabriel6181997@gmail.com>
2021-10-18 18:10:29 +0900

    ✨ Use dotenv

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
 const express = require("express");
 const app = express();
-const cors = require("cors")
+const cors = require("cors");
+require("dotenv").config();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 const db = require("./models");
 
 //Routers
-const postRouter = require("./routes/Posts")
+const postRouter = require("./routes/Posts");
 app.use("/posts", postRouter);
-const commentRouter = require("./routes/Comments")
+const commentRouter = require("./routes/Comments");
 app.use("/comments", commentRouter);
-const usersRouter = require("./routes/Users")
+const usersRouter = require("./routes/Users");
 app.use("/auth", usersRouter);
-const likesRouter = require("./routes/Likes")
+const likesRouter = require("./routes/Likes");
 app.use("/likes", likesRouter);
 
-
-db.sequelize.sync().then(() => {
-  app.listen(3001, () => {
-    console.log("Server running on port 3001");
+db.sequelize
+  .sync()
+  .then(() => {
+    app.listen(process.env.PORT || 3001, () => {
+      console.log("Server running on port 3001");
+    });
+  })
+  .catch((error) => {
+    console.log(error);
   });
-});
